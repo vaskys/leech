ba66180a750e781aa86af95ba9f853a15c3b1179
cmon bruh <cotiajviemmladyxD@eksdi.com>
2023-04-04 15:39:13 +0200

    Initial Commit

---
+++ Leech/LeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeechApp.swift
+//  Leech
+//
+//  Created by Samo Vaský on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
